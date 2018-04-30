0a820fd969067585d17d680227706776090f9951
Rian Halperin <rianhalperin@gmail.com>
2018-04-30 16:31:46 -0400

    feat(react): plug router, app component into client

--- client/index.js
+++ client/index.js
@@ -2,11 +2,16 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import { Provider } from 'react-redux'
 import store from './store'
+import { Router } from 'react-router-dom'
+import history from './history'
+import App from './app'
 
 
 ReactDOM.render(
   <Provider store={store}>
-    <div>hi!</div>
+    <Router history={history}>
+      <App />
+    </Router>
   </Provider>,
   document.getElementById('app')
 )
