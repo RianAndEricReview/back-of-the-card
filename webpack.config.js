0f9983d124436210849c3cb52ff48225781e5f0a
Eric Garnett <egcodes@gmail.com>
2018-05-01 10:35:58 -0400

    feat(css): create webpack entry point

--- webpack.config.js
+++ webpack.config.js
@@ -18,6 +18,13 @@ module.exports = {
         test: /\.jsx?$/,
         exclude: /node_modules/,
         loader: 'babel-loader'
+      },
+      {
+        test: /\.css$/,
+        use: [
+          'style-loader',
+          'css-loader',
+        ]
       }
     ]
   }
