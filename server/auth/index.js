d45f5d79df1006bb112573f8da52d2da3288e735
Eric Garnett <egcodes@gmail.com>
2018-05-01 16:49:17 -0400

    fix(routes): edit typos

--- server/auth/index.js
+++ server/auth/index.js
@@ -1,5 +1,5 @@
 const router = require('express').Router()
-const { User } = require('../models')
+const { User } = require('../db/models')
 module.exports = router
 
 router.post('/index', (req, res, next) => {
