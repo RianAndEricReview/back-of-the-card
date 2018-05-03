627cef129c6d34a467113ad7f2aaa87276bfb585
Eric Garnett <egcodes@gmail.com>
2018-05-03 11:10:53 -0400

    feat(api): initial user GET route test

---
+++ tests/serverTesting/user.spec.js
@@ -0,0 +1,22 @@
+const { expect } = require('chai')
+const request = require('supertest')
+const db = require('../../server/db')
+const app = require('../../server')
+const User = db.model('user')
+
+describe('User routes', () => {
+  beforeEach(() => db.sync({ force: true }))
+
+  describe('/api/users/', () => {
+    const bobsEmail = 'bob@bob.bob'
+
+    beforeEach(() => User.create({ email: bobsEmail }))
+    it('GET /api/users', () => {
+      return request(app).get('/api/users').expect(200)
+        .then(res => {
+          expect(res.body).to.be.an('array')
+          expect(res.body[0].email).to.be.equal(bobsEmail)
+        })
+    })
+  })
+})
