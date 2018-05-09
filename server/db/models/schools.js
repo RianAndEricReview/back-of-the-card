3afc2bba74e56964f3f0da9e11554c6f955080b6
Rian Halperin <rianhalperin@gmail.com>
2018-05-09 09:58:00 -0400

    feat(schools) create model

---
+++ server/db/models/schools.js
@@ -0,0 +1,34 @@
+const Sequelize = require('sequelize')
+const db = require('../db')
+
+
+const Schools = db.define('schools', {
+  schoolID: {
+    type: Sequelize.INTEGER,
+    primaryKey: true,
+  },
+  name_full: {
+    type: Sequelize.STRING,
+  },
+  city: {
+    type: Sequelize.STRING,
+  },
+  state: {
+    type: Sequelize.STRING,
+  },
+  country: {
+    type: Sequelize.STRING,
+  },
+  createdAt: {
+    type: Sequelize.DATE,
+    field: 'created_at',
+    defaultValue: Sequelize.fn('NOW')
+  },
+  updatedAt: {
+    type: Sequelize.DATE,
+    field: 'updated_at',
+    defaultValue: Sequelize.fn('NOW')
+  }
+}, {timestamps: true})
+
+module.exports = Schools
