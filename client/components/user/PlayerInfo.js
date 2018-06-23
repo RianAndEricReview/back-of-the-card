4f591fa6039f1054bdb02ed83a8428a5f3e195fb
Eric Garnett <egcodes@gmail.com>
2018-06-23 12:35:06 -0400

    feat(playerImages): create playerImages directory

--- client/components/user/PlayerInfo.js
+++ client/components/user/PlayerInfo.js
@@ -4,17 +4,22 @@ import PropTypes from 'prop-types'
 import { setPlayerInfoThunk } from '../../store'
 
 const PlayerInfo = props => {
-  const { handleSubmit, error, userId } = props
+  const { handleSubmit, error, userId, screenName } = props
   return (
     <div id="player-info-form">
       <form onSubmit={(event) => handleSubmit(event, userId)}>
-      <div>
-        <h3>Choose Player Name & Image</h3>
-      </div>
+        <div>
+          <h3>Choose Player Name & Image</h3>
+        </div>
         <div className="signup-fields">
           <label htmlFor="screenName"><small>Screen Name</small></label>
-          <input name="screenName" type="text" />
+          <input name="screenName" type="text" placeholder={screenName} />
         </div>
+
+        <div>
+          <img src="../../../playerImages/playerImage_1.jpg" />
+        </div>
+
         <div id="player-info-submit">
           <button type="submit" className="btn btn-success">Submit</button>
         </div>
@@ -27,7 +32,8 @@ const PlayerInfo = props => {
 const mapState = state => {
   return {
     userId: state.user.id,
-    error: state.user.error
+    error: state.user.error,
+    screenName: state.user.screenName
   }
 }
 
