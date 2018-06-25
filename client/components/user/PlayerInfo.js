6b297f7df647a3cef8b8286b62dfd707c2fe86be
Eric Garnett <egcodes@gmail.com>
2018-06-25 10:37:15 -0400

    chore(playerImage): add comments regarding future dynamic image plans

--- client/components/user/PlayerInfo.js
+++ client/components/user/PlayerInfo.js
@@ -5,6 +5,7 @@ import { setPlayerInfoThunk } from '../../store'
 
 const PlayerInfo = props => {
   const { handleSubmit, error, userId, screenName } = props
+  // This code generates a static/fixed number of images to display...In the future we will try to find a way to read the playerImage directory to generate all potential player images dynamically
   const imageArray = []
   for (let i = 0; i <= 5; i++) {
     imageArray.push({ path: `../../../playerImages/playerImage_${i}.jpg`, key: i})
