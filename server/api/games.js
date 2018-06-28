da45119dc3b549b8fc84d40005265dda994289e2
Eric Garnett <egcodes@gmail.com>
2018-06-28 12:47:17 -0400

    feat(game): implement initial add player to game route

--- server/api/games.js
+++ server/api/games.js
@@ -4,8 +4,18 @@ module.exports = router
 
 router.post('/', (req, res, next) => {
   Game.create(req.body)
-  .then(game => {
-    res.status(201).json(game)
-  })
-  .catch(next)
+    .then(game => {
+      res.status(201).json(game)
+    })
+    .catch(next)
+})
+
+router.put('/:gameId/addNewPlayer', (req, res, next) => {
+  Game.findById(req.params.gameId)
+    .then(game => {
+      const playersArray = [...game.players, req.body.id]
+      return game.update({players: playersArray})
+    })
+    .then(game => res.status(201).json(game))
+    .catch(next)
 })
