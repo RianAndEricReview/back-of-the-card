fcc4b2192e971ff6b01edb0e61fde3c6ab2048a2
Eric Garnett <egcodes@gmail.com>
2018-06-28 12:56:40 -0400

    fix(game): delete put route accidentally merged to master

--- server/api/games.js
+++ server/api/games.js
@@ -10,16 +10,3 @@ router.post('/', (req, res, next) => {
     .catch(next)
 })
 
-router.put('/:gameId/addNewPlayer', (req, res, next) => {
-  Game.findById(req.params.gameId)
-    .then(game => {
-      const playersArray = [...game.players, req.body.id]
-      if (playersArray.length === game.maxPlayers) {
-        return game.update({players: playersArray, open: false})
-      } else {
-        return game.update({players: playersArray})
-      }
-    })
-    .then(game => res.status(201).json(game))
-    .catch(next)
-})
