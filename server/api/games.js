819390c2094a9654411dc83064965c528af995a2
Eric Garnett <egcodes@gmail.com>
2018-06-28 12:09:26 -0400

    feat(game): create backend post route

---
+++ server/api/games.js
@@ -0,0 +1,11 @@
+const router = require('express').Router()
+const { Game } = require('../db/models')
+module.exports = router
+
+router.post('/', (req, res, next) => {
+  Game.create(req.body)
+  .then(game => {
+    res.status(201).json(game)
+  })
+  .catch(next)
+})
