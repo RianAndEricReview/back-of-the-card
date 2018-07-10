fa04e160fa710be5e16dbec00456289734047202
Rian Halperin <rianhalperin@gmail.com>
2018-07-10 11:10:13 -0400

    feat(game) create initial game components

--- client/components/game/GameContainer.js
+++ client/components/game/GameContainer.js
@@ -1,9 +1,46 @@
 import React, { Component } from 'react'
+import { connect } from 'react-redux'
+import { withRouter } from 'react-router-dom'
+import LoadingPres from './LoadingPres'
+import IndividualPlayerPres from './IndividualPlayerPres'
+import GameBoardPres from './GameBoardPres'
+import { getGameThunk } from '../../store'
 
-export default () => {
-  return (
-    <div>You have entered a game!</div>
-  )
+export class GameContainerClass extends Component {
+  constructor(props) {
+    super(props)
+
+  }
+
+
+  componentDidMount() {
+
+  }
+
+  componentWillUnmount() {
+
+  }
+
+  render() {
+    return (
+      <div>
+        <LoadingPres />
+        <div>
+          <IndividualPlayerPres />
+        </div>
+      </div>
+    )
+  }
 }
 
+const mapStateToProps = state => ({
+  user: state.user,
+  game: state.game,
+})
 
+const mapDispatchToProps = dispatch => {
+
+}
+
+const GameContainer = withRouter(connect(mapStateToProps, mapDispatchToProps)(GameContainerClass))
+export default GameContainer
---
+++ client/components/game/LoadingPres.js
@@ -0,0 +1,15 @@
+import React from 'react'
+/* eslint-disable react/display-name */
+export default () => {
+  return (
+    <div className="landing-pres-container">
+      <div id="landing-content-container">
+        <div id="landing-content">
+          <div>
+            <h1>Waiting for Players...</h1>
+          </div>
+        </div>
+      </div>
+    </div>
+  )
+}
