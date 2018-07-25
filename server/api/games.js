8a6b7f783976adbb098312233091bf7c6aef9feb
Rian Halperin <rianhalperin@gmail.com>
2018-07-25 13:42:44 -0400

    refactor(games): update add player put route nested

--- server/api/games.js
+++ server/api/games.js
@@ -1,4 +1,5 @@
 const router = require('express').Router()
+const Bluebird = require('bluebird')
 const { Game, Gametype, GamePlayer } = require('../db/models')
 module.exports = router
 
@@ -14,7 +15,7 @@ router.get('/gametypes', (req, res, next) => {
 // Used to find open game instance for particular game type
 router.get('/:gametypeId', (req, res, next) => {
   Game.findOne({
-    where: {open: true, gametypeId: req.params.gametypeId}
+    where: { open: true, gametypeId: req.params.gametypeId }
   })
     .then(game => {
       res.status(201).json(game)
@@ -33,16 +34,30 @@ router.post('/', (req, res, next) => {
 })
 
 // Used to add a player to a game instance
+// router.put('/:gameId/addNewPlayer', (req, res, next) => {
+//   const gamePromise = Game.findById(req.params.gameId)
+//   const findPlayersPromise = GamePlayer.findAll({ where: { gameId: req.params.gameId } })
+//   GamePlayer.create({ gameId: req.params.gameId, userId: req.body.playerId.toString() })
+//     .then(() => {
+//       GamePlayer.findAll({ where: { gameId: req.params.gameId } })
+//         .then((result) => {
+//           res.status(200).json(result)
+//         })
+//     })
+//     .catch(next)
+// })
+
 router.put('/:gameId/addNewPlayer', (req, res, next) => {
-  const gamePromise = Game.findById(req.params.gameId)
-  const gamePlayerPromise = GamePlayer.create({gameId: req.params.gameId, userId: req.body.playerId.toString()})
-  const findPlayersPromise = GamePlayer.findAll({where: {gameId: req.params.gameId}})
-  Promise.map([gamePromise, gamePlayerPromise, findPlayersPromise], result => result)
-  .then((game, newPlayer, players) => {
-    res.status(200).json(players)
-    if (players.length === game.gametype.maxPlayers){
-      game.update({ open: false })
-      }
-  })
-  .catch(next)
+  GamePlayer.create({ gameId: req.params.gameId, userId: req.body.playerId.toString() })
+    .then(() => {
+      return Promise.all([Game.findById(req.params.gameId), GamePlayer.findAll({ where: { gameId: req.params.gameId } })])
+        .then((result) => {
+          const [game, players] = result
+          if (players.length === game.gametype.maxPlayers) {
+            game.update({ open: false })
+          }
+          res.status(200).json(players)
+        })
+    })
+    .catch(next)
 })
