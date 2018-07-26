2255db020f90a8f84004187f922be1e1bf72b368
Rian Halperin <rianhalperin@gmail.com>
2018-07-26 10:27:42 -0400

    chore(games): add comments to put route

--- server/api/games.js
+++ server/api/games.js
@@ -34,22 +34,11 @@ router.post('/', (req, res, next) => {
 })
 
 // Used to add a player to a game instance
-// router.put('/:gameId/addNewPlayer', (req, res, next) => {
-//   const gamePromise = Game.findById(req.params.gameId)
-//   const findPlayersPromise = GamePlayer.findAll({ where: { gameId: req.params.gameId } })
-//   GamePlayer.create({ gameId: req.params.gameId, userId: req.body.playerId.toString() })
-//     .then(() => {
-//       GamePlayer.findAll({ where: { gameId: req.params.gameId } })
-//         .then((result) => {
-//           res.status(200).json(result)
-//         })
-//     })
-//     .catch(next)
-// })
-
 router.put('/:gameId/addNewPlayer', (req, res, next) => {
+  //Create the GamePlayer instance linking user to game.
   GamePlayer.create({ gameId: req.params.gameId, userId: req.body.playerId.toString() })
     .then(() => {
+      //Get info to decide whether to close game or not.
       return Promise.all([Game.findById(req.params.gameId), GamePlayer.findAll({ where: { gameId: req.params.gameId } })])
         .then((result) => {
           const [game, players] = result
