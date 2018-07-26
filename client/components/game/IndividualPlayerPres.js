0901e5577f3472c1c60a450deb8bae498ae40c0c
Rian Halperin <rianhalperin@gmail.com>
2018-07-26 13:28:54 -0400

    refactor(indPlayerPres): update image and screenName

--- client/components/game/IndividualPlayerPres.js
+++ client/components/game/IndividualPlayerPres.js
@@ -4,11 +4,11 @@ export default (props) => {
   return (
     <div className="player-pres-container">
       <div className="player-content-image">
-        <img src={props.user.playerImage} />
+        <img src={props.player.user.playerImage} />
       </div>
       <div className="player-content-container">
         <div className="player-content">
-          <h4>Player: {props.user.screenName}</h4>
+          <h4>Player: {props.player.user.screenName}</h4>
         </div>
         <div className="player-content">
           <h4>Score: 7224</h4>
