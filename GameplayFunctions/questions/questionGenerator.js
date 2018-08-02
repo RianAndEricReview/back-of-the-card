84740dd8ae90ff57e6989701f67ed4297f37cc75
Rian Halperin <rianhalperin@gmail.com>
2018-08-02 14:12:28 -0400

    feat(questions): expand on question framework

--- GameplayFunctions/questions/questionGenerator.js
+++ GameplayFunctions/questions/questionGenerator.js
@@ -1,5 +1,13 @@
-const questionTypeArray = [{ value: 'hitting', weight: 4 }, { value: 'pitching', weight: 3 }, { value: 'managers', weight: 1 }]
+// example arrays for questionChoice functions
+// const seasonOrCareerArray = [{ value: 'singleSeason', weight: 4, nextChoice: teamPlayerArray}, { value: 'career', weight: 3 }, { value: 'managers', weight: 1 }]
+// const teamOrPlayerArray = [{ value: 'team', weight: 4, nextChoice: [team, player]}, { value: 'player', weight: 4, nextChoice: [team, player]}]
+// const playerCategories = [{ value: 'singleSeason', weight: 4, nextChoice: 4}]
+// const teamCategories = 
+// const stat = [{ value: 'homerun', weight: 4}]
 
+
+//this helper function is used to pick a random value from an array based on weight prop of object
+//will be moved to a different file
 const randomValueSelector = (arr) => {
   const totalWeight = arr.reduce((accum, currObj) => {
     return accum + currObj.weight
@@ -11,12 +19,12 @@ const randomValueSelector = (arr) => {
   for (let i = 0; i < arr.length; i++) {
     currentNum += arr[i].weight
     if (targetNum <= currentNum) {
-      return arr[i].value
+      return arr[i]
     }
   }
 }
 
-
+//this class will use the QuestionChoices object to build the question object
 class Question {
   constructor() {
     this.questionText = ''
@@ -24,7 +32,38 @@ class Question {
     this.answerChoices = []
   }
 
-  questionGenerator() {
-    this.questionType = randomValueSelector(questionTypeArray)
+}
+
+//class to create an object with a list of question pieces
+class QuestionChoices {
+  constructor() {
+    this.phrasing = []
+  }
+
+  questionChoiceGenerator(startFunc){
+    startFunc()
+  }
+}
+
+
+//placeholder functions to be called by questionChoiceGenerator, will be moved to own file.
+const seasonOrCareerArray = () => {
+  const seasonCareerArray = [{ value: 'singleSeason', weight: 4, nextChoice: teamPlayerArray, phrasing: false}, { value: 'career', weight: 3 }, { value: 'managers', weight: 1 }]
+  const obj = randomValueSelector(seasonOrCareerArray)
+  this.seasonOrCareer = obj.value
+  if (obj.phrasing){
+    this.phrasing.push(obj.value)
+  }
+  if (obj.nextChoice){
+    obj.nextChoice()
+  }
+}
+
+const teamPlayerArray = () => {
+  const teamOrPlayerArray = [{ value: 'team', weight: 4, nextChoice: [team, player]}, { value: 'player', weight: 4, nextChoice: [team, player]}]
+  const obj = randomValueSelector(seasonOrCareerArray)
+  this.seasonOrCareer = obj.value
+  if(obj.nextChoice){
+    obj.nextChoice()
   }
 }
---
+++ GameplayFunctions/questions/questionSkeletons.js
@@ -0,0 +1,10 @@
+//very rough, placeholder question skeletons. Probably won't need phrasing array.
+const questionCat1 = {
+  phrasing: ['homerun', 'singleYear', 'most', 'league'],
+  text: [`who led the ${this.league} in homeruns in ${this.year}?`, `In ${this.year}, which player smacked the most dingers in the ${this.league}`]
+}
+
+const question2 = {
+  phrasing: ['homeruns', 'yearRange', 'most']
+}
+
