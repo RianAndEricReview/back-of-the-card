dae1baf6051164389a475ae15af36a951afe0d94
Rian Halperin <rianhalperin@gmail.com>
2018-08-02 11:30:08 -0400

    feat(questionGenerator): create class constructor framework

---
+++ GameplayFunctions/questions/questionGenerator.js
@@ -0,0 +1,11 @@
+class Question {
+  constructor() {
+    this.questionText = ''
+    this.correctAnswer = ''
+    this.answerChoices = []
+  }
+
+  questionGenerator() {
+    this.questionType = ''
+  }
+}
