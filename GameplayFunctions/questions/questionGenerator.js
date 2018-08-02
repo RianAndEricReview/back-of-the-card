c5dba71d9dfbe90701ea6aae18b7ed4e25b8c94b
Rian Halperin <rianhalperin@gmail.com>
2018-08-02 11:31:57 -0400

    feat(questions): create weighted random value selector helper function

--- GameplayFunctions/questions/questionGenerator.js
+++ GameplayFunctions/questions/questionGenerator.js
@@ -1,3 +1,22 @@
+const questionTypeArray = [{ value: 'hitting', weight: 4 }, { value: 'pitching', weight: 3 }, { value: 'managers', weight: 1 }]
+
+const randomValueSelector = (arr) => {
+  const totalWeight = arr.reduce((accum, currObj) => {
+    return accum + currObj.weight
+  }, 0)
+
+  let currentNum = 0;
+  const targetNum = Math.floor(Math.random() * totalWeight) + 1
+
+  for (let i = 0; i < arr.length; i++) {
+    currentNum += arr[i].weight
+    if (targetNum <= currentNum) {
+      return arr[i].value
+    }
+  }
+}
+
+
 class Question {
   constructor() {
     this.questionText = ''
@@ -6,6 +25,6 @@ class Question {
   }
 
   questionGenerator() {
-    this.questionType = ''
+    this.questionType = randomValueSelector(questionTypeArray)
   }
 }
