a58471fde9f7ee89b2ec3989906149ca098e6cf5
Eric Garnett <egcodes@gmail.com>
2018-08-09 13:39:27 -0400

    ref(questions): update question objects with skeleton keys

--- GameplayFunctions/questions/questionOptions.js
+++ GameplayFunctions/questions/questionOptions.js
@@ -3,62 +3,64 @@
 //reminder, order matters. The first choice needs to be at the end of the file. The first choice also needs to be exported.
 
 const mostOrLeast = [
-  {whatToSet: [{key: 'mostOrLeast', value: 'most', neededToSelectQuestion: true}], weight: 3},
-  {whatToSet: [{key: 'mostOrLeast', value: 'least', neededToSelectQuestion: true}], weight: 1}
+  {whatToSet: [{key: 'mostOrLeast', value: 'most', skeletonType: {stat: {mostOrLeast: [`most`]}}}], weight: 3},
+  {whatToSet: [{key: 'mostOrLeast', value: 'least', skeletonType: {stat: {mostOrLeast: [`least`]}}}], weight: 1}
 ]
 
 const overallBattingStats = [
-  {whatToSet: [{key: 'statCategory', value: 'HOMERUNS', neededToSelectQuestion: true}, {key: 'mostOrLeast', value: 'most', neededToSelectQuestion: true}], weight: 8},
-  {whatToSet: [{key: 'statCategory', value: 'BASEHITS', neededToSelectQuestion: true}, {key: 'mostOrLeast', value: 'most', neededToSelectQuestion: true}], weight: 3},
-  {whatToSet: [{key: 'statCategory', value: 'DOUBLES', neededToSelectQuestion: true}, {key: 'mostOrLeast', value: 'most', neededToSelectQuestion: true}], weight: 3},
-  {whatToSet: [{key: 'statCategory', value: 'TRIPLES', neededToSelectQuestion: true}, {key: 'mostOrLeast', value: 'most', neededToSelectQuestion: true}], weight: 3},
-  {whatToSet: [{key: 'statCategory', value: 'XTRABASEHITS', neededToSelectQuestion: true}, {key: 'mostOrLeast', value: 'most', neededToSelectQuestion: true}], weight: 2},
-  {whatToSet: [{key: 'statCategory', value: 'RBI', neededToSelectQuestion: true}, {key: 'mostOrLeast', value: 'most', neededToSelectQuestion: true}], weight: 5},
-  {whatToSet: [{key: 'statCategory', value: 'ATBATS', neededToSelectQuestion: true}, {key: 'mostOrLeast', value: 'most', neededToSelectQuestion: true}], weight: 2},
-  {whatToSet: [{key: 'statCategory', value: 'WALKS', neededToSelectQuestion: true}], weight: 3, nextChoice: mostOrLeast},
-  {whatToSet: [{key: 'statCategory', value: 'RUNS', neededToSelectQuestion: true}, {key: 'mostOrLeast', value: 'most', neededToSelectQuestion: true}], weight: 3},
-  {whatToSet: [{key: 'statCategory', value: 'TOTALBASES', neededToSelectQuestion: true}, {key: 'mostOrLeast', value: 'most', neededToSelectQuestion: true}], weight: 2},
-  {whatToSet: [{key: 'statCategory', value: 'STOLENBASES', neededToSelectQuestion: true}, {key: 'mostOrLeast', value: 'most', neededToSelectQuestion: true}], weight: 3},
-  {whatToSet: [{key: 'statCategory', value: 'STRIKINGOUT', neededToSelectQuestion: true}], weight: 3, nextChoice: mostOrLeast},
-  {whatToSet: [{key: 'statCategory', value: 'HITBYPITCH', neededToSelectQuestion: true}, {key: 'mostOrLeast', value: 'most', neededToSelectQuestion: true}], weight: 1},
-  {whatToSet: [{key: 'statCategory', value: 'INTENTIONALWALKS', neededToSelectQuestion: true}, {key: 'mostOrLeast', value: 'most', neededToSelectQuestion: true}], weight: 1},
-  {whatToSet: [{key: 'statCategory', value: 'DOUBLEPLAYS', neededToSelectQuestion: true}], weight: 1, nextChoice: mostOrLeast},
+  {whatToSet: [{key: 'statCategory', value: 'HOMERUNS', skeletonType: {stat: {verb: [`hit`], objectNoun: [`home runs`]}}}, {key: 'mostOrLeast', value: 'most', skeletonType: {stat: {mostOrLeast: [`most`]}}}], weight: 8},
+  {whatToSet: [{key: 'statCategory', value: 'BASEHITS', skeletonType: {stat: {verb: [`had`], objectNoun: [`base hits`]}}}, {key: 'mostOrLeast', value: 'most', skeletonType: {stat: {mostOrLeast: [`most`]}}}], weight: 3},
+  {whatToSet: [{key: 'statCategory', value: 'DOUBLES', skeletonType: {stat: {verb: [`hit`], objectNoun: [`doubles`]}}}, {key: 'mostOrLeast', value: 'most', skeletonType: {stat: {mostOrLeast: [`most`]}}}], weight: 3},
+  {whatToSet: [{key: 'statCategory', value: 'TRIPLES', skeletonType: {stat: {verb: [`legged out`], objectNoun: [`triples`]}}}, {key: 'mostOrLeast', value: 'most', skeletonType: {stat: {mostOrLeast: [`most`]}}}], weight: 3},
+  {whatToSet: [{key: 'statCategory', value: 'XTRABASEHITS', skeletonType: {stat: {verb: [`had`], objectNoun: [`extra base hits`]}}}, {key: 'mostOrLeast', value: 'most', skeletonType: {stat: {mostOrLeast: [`most`]}}}], weight: 2},
+  {whatToSet: [{key: 'statCategory', value: 'RBI', skeletonType: {stat: {verb: [`had`], objectNoun: [`RBIs`]}}}, {key: 'mostOrLeast', value: 'most', skeletonType: {stat: {mostOrLeast: [`most`]}}}], weight: 5},
+  {whatToSet: [{key: 'statCategory', value: 'ATBATS', skeletonType: {stat: {verb: [`had`], objectNoun: [`at bats`]}}}, {key: 'mostOrLeast', value: 'most', skeletonType: {stat: {mostOrLeast: [`most`]}}}], weight: 2},
+  {whatToSet: [{key: 'statCategory', value: 'WALKS', skeletonType: {stat: {verb: [`had`], objectNoun: [`walks`]}}}], weight: 3, nextChoice: mostOrLeast},
+  {whatToSet: [{key: 'statCategory', value: 'RUNS', skeletonType: {stat: {verb: [`scored`], objectNoun: [`runs`]}}}, {key: 'mostOrLeast', value: 'most', skeletonType: {stat: {mostOrLeast: [`most`]}}}], weight: 3},
+  {whatToSet: [{key: 'statCategory', value: 'TOTALBASES', skeletonType: {stat: {verb: [`had`], objectNoun: [`total bases`]}}}, {key: 'mostOrLeast', value: 'most', skeletonType: {stat: {mostOrLeast: [`most`]}}}], weight: 2},
+  {whatToSet: [{key: 'statCategory', value: 'STOLENBASES', skeletonType: {stat: {verb: [`stole`], objectNoun: [`bases`]}}}, {key: 'mostOrLeast', value: 'most', skeletonType: {stat: {mostOrLeast: [`most`]}}}], weight: 3},
+  {whatToSet: [{key: 'statCategory', value: 'STRIKINGOUT', skeletonType: {stat: {verb: [`struck out`]}}}], weight: 3, nextChoice: mostOrLeast},
+  {whatToSet: [{key: 'statCategory', value: 'HITBYPITCH', skeletonType: {stat: {verb: [`was hit by a pitch`]}}}, {key: 'mostOrLeast', value: 'most', skeletonType: {stat: {mostOrLeast: [`most`]}}}], weight: 1},
+  {whatToSet: [{key: 'statCategory', value: 'INTENTIONALWALKS', skeletonType: {stat: {verb: [`was walked intentionally`], }}}, {key: 'mostOrLeast', value: 'most', skeletonType: {stat: {mostOrLeast: [`most`]}}}], weight: 1},
+  {whatToSet: [{key: 'statCategory', value: 'DOUBLEPLAYS', skeletonType: {stat: {verb: [`hit into`], objectNoun: [`double plays`]}}}], weight: 1, nextChoice: mostOrLeast},
 ]
 
 const comparisonBattingStats = [
-  {whatToSet: [{key: 'statCategory', value: 'HOMERUNS', neededToSelectQuestion: true}], weight: 8, nextChoice: mostOrLeast},
-  {whatToSet: [{key: 'statCategory', value: 'BASEHITS', neededToSelectQuestion: true}], weight: 3, nextChoice: mostOrLeast},
-  {whatToSet: [{key: 'statCategory', value: 'DOUBLES', neededToSelectQuestion: true}], weight: 3, nextChoice: mostOrLeast},
-  {whatToSet: [{key: 'statCategory', value: 'TRIPLES', neededToSelectQuestion: true}], weight: 3, nextChoice: mostOrLeast},
-  {whatToSet: [{key: 'statCategory', value: 'XTRABASEHITS', neededToSelectQuestion: true}], weight: 2, nextChoice: mostOrLeast},
-  {whatToSet: [{key: 'statCategory', value: 'RBI', neededToSelectQuestion: true}], weight: 5, nextChoice: mostOrLeast},
-  {whatToSet: [{key: 'statCategory', value: 'ATBATS', neededToSelectQuestion: true}], weight: 2, nextChoice: mostOrLeast},
-  {whatToSet: [{key: 'statCategory', value: 'WALKS', neededToSelectQuestion: true}], weight: 3, nextChoice: mostOrLeast},
-  {whatToSet: [{key: 'statCategory', value: 'RUNS', neededToSelectQuestion: true}], weight: 3, nextChoice: mostOrLeast},
-  {whatToSet: [{key: 'statCategory', value: 'TOTALBASES', neededToSelectQuestion: true}], weight: 2, nextChoice: mostOrLeast},
-  {whatToSet: [{key: 'statCategory', value: 'STOLENBASES', neededToSelectQuestion: true}], weight: 3, nextChoice: mostOrLeast},
-  {whatToSet: [{key: 'statCategory', value: 'STRIKINGOUT', neededToSelectQuestion: true}], weight: 3, nextChoice: mostOrLeast},
-  {whatToSet: [{key: 'statCategory', value: 'HITBYPITCH', neededToSelectQuestion: true}, {key: 'mostOrLeast', value: 'most', neededToSelectQuestion: true}], weight: 1},
-  {whatToSet: [{key: 'statCategory', value: 'INTENTIONALWALKS', neededToSelectQuestion: true}, {key: 'mostOrLeast', value: 'most', neededToSelectQuestion: true}], weight: 1},
-  {whatToSet: [{key: 'statCategory', value: 'DOUBLEPLAYS', neededToSelectQuestion: true}], weight: 1, nextChoice: mostOrLeast},
+  {whatToSet: [{key: 'statCategory', value: 'HOMERUNS', skeletonType: {stat: {verb: [`hit`], objectNoun: [`home runs`]}}}], weight: 8, nextChoice: mostOrLeast},
+  {whatToSet: [{key: 'statCategory', value: 'BASEHITS', skeletonType: {stat: {verb: [`had`], objectNoun: [`base hits`]}}}], weight: 3, nextChoice: mostOrLeast},
+  {whatToSet: [{key: 'statCategory', value: 'DOUBLES', skeletonType: {stat: {verb: [`hit`], objectNoun: [`doubles`]}}}], weight: 3, nextChoice: mostOrLeast},
+  {whatToSet: [{key: 'statCategory', value: 'TRIPLES', skeletonType: {stat: {verb: [`legged out`], objectNoun: [`triples`]}}}], weight: 3, nextChoice: mostOrLeast},
+  {whatToSet: [{key: 'statCategory', value: 'XTRABASEHITS', skeletonType: {stat: {verb: [`had`], objectNoun: [`extra base hits`]}}}], weight: 2, nextChoice: mostOrLeast},
+  {whatToSet: [{key: 'statCategory', value: 'RBI', skeletonType: {stat: {verb: [`had`], objectNoun: [`RBIs`]}}}], weight: 5, nextChoice: mostOrLeast},
+  {whatToSet: [{key: 'statCategory', value: 'ATBATS', skeletonType: {stat: {verb: [`had`], objectNoun: [`at bats`]}}}], weight: 2, nextChoice: mostOrLeast},
+  {whatToSet: [{key: 'statCategory', value: 'WALKS', skeletonType: {stat: {verb: [`had`], objectNoun: [`walks`]}}}], weight: 3, nextChoice: mostOrLeast},
+  {whatToSet: [{key: 'statCategory', value: 'RUNS', skeletonType: {stat: {verb: [`scored`], objectNoun: [`runs`]}}}], weight: 3, nextChoice: mostOrLeast},
+  {whatToSet: [{key: 'statCategory', value: 'TOTALBASES', skeletonType: {stat: {verb: [`had`], objectNoun: [`total bases`]}}}], weight: 2, nextChoice: mostOrLeast},
+  {whatToSet: [{key: 'statCategory', value: 'STOLENBASES', skeletonType: {stat: {verb: [`stole`], objectNoun: [`bases`]}}}], weight: 3, nextChoice: mostOrLeast},
+  {whatToSet: [{key: 'statCategory', value: 'STRIKINGOUT', skeletonType: {stat: {verb: [`struck out`]}}}], weight: 3, nextChoice: mostOrLeast},
+  {whatToSet: [{key: 'statCategory', value: 'HITBYPITCH', skeletonType: {stat: {verb: [`was hit by a pitch`]}}}, {key: 'mostOrLeast', value: 'most', skeletonType: {stat: {mostOrLeast: [`most`]}}}], weight: 1},
+  {whatToSet: [{key: 'statCategory', value: 'INTENTIONALWALKS', skeletonType: {stat: {verb: [`was walked intentionally`]}}}, {key: 'mostOrLeast', value: 'most', skeletonType: {stat: {mostOrLeast: [`most`]}}}], weight: 1},
+  {whatToSet: [{key: 'statCategory', value: 'DOUBLEPLAYS', skeletonType: {stat: {verb: [`hit into`], objectNoun: [`double plays`]}}}], weight: 1, nextChoice: mostOrLeast},
 ]
+
+const overallTimeFrame = [
+  {whatToSet: [{key: 'timeFrame', value: 'singleSeason', skeletonType: {stat: {timeFrame: [`in ${year}`]}}}], weight: 2, nextChoice: overallBattingStats},
+  {whatToSet: [{key: 'timeFrame', value: 'allTime', skeletonType: {stat: {timeFrame: [`all time`]}}}], weight: 1, nextChoice: overallBattingStats}
+]
+
+const comparisonTimeFrame = [
+  {whatToSet: [{key: 'timeFrame', value: 'singleSeason', skeletonType: {stat: {timeFrame: [`in ${year}`]}}}], weight: 3, nextChoice: comparisonBattingStats},
+  {whatToSet: [{key: 'timeFrame', value: 'allTime', weight: 1}], nextChoice: comparisonBattingStats}
+]
+
 
 const questionType = [
-  {whatToSet: [{key: 'questionType', value: 'comparison', neededToSelectQuestion: true}], weight: 1, nextChoice: comparisonBattingStats},
-  {whatToSet: [{key: 'questionType', value: 'overall', neededToSelectQuestion: true}], weight: 1, nextChoice: overallBattingStats}
+  {whatToSet: [{key: 'questionType', value: 'comparison', skeletonType: {stat: {comparativePhrasing: [`of these`], pluralization: [`s`]}}}], weight: 1, nextChoice: comparisonTimeFrame},
+  {whatToSet: [{key: 'questionType', value: 'overall'}], weight: 1, nextChoice: overallTimeFrame}
 ]
 
-const teamTimeFrame = [
-  {whatToSet: [{key: 'timeFrame', value: 'singleSeason', neededToSelectQuestion: true}], weight: 2, nextChoice: questionType},
-  {whatToSet: [{key: 'timeFrame', value: 'franchiseHistory', neededToSelectQuestion: true}], weight: 1, nextChoice: questionType}
-]
-
-const playerTimeFrame = [
-  {whatToSet: [{key: 'timeFrame', value: 'singleSeason', neededToSelectQuestion: true}], weight: 3, nextChoice: questionType},
-  {whatToSet: [{key: 'timeFrame', value: 'wholeCareer', neededToSelectQuestion: true}], weight: 1, nextChoice: questionType}
-]
 
 export const teamOrPlayer = [
-  {whatToSet: [{key: 'teamOrPlayer', value: 'singlePlayer', skeletonType: {stat: {subjectNoun: ['player']}}}], weight: 4, nextChoice: playerTimeFrame},
-  {whatToSet: [{key: 'teamOrPlayer', value: 'wholeTeam', skeletonType: {stat: {subjectNoun: ['team']}}}], weight: 2, nextChoice: teamTimeFrame}
+  {whatToSet: [{key: 'teamOrPlayer', value: 'singlePlayer', skeletonType: {stat: {subjectNoun: [`player`]}}}], weight: 4, nextChoice: questionType},
+  {whatToSet: [{key: 'teamOrPlayer', value: 'wholeTeam', skeletonType: {stat: {subjectNoun: [`team`]}}}], weight: 2, nextChoice: questionType}
 ]
