f61f62030a113f65a4334ef0b9a555ae2b19a7b5
Rian Halperin <rianhalperin@gmail.com>
2018-08-13 14:30:17 -0400

    chore(question content): move year & option content to content folder

--- GameplayFunctions/questions/questionHelperFuncs.js
+++ GameplayFunctions/questions/questionHelperFuncs.js
@@ -23,15 +23,3 @@ export const randomYearSelector = (years) => {
 
   return Math.floor(Math.random() * (yearRange.end - yearRange.start + 1)) + yearRange.start
 }
-
-
-export const yearRangesArr = [
-  {start: 1871, end: 1909, weight: 2},
-  {start: 1910, end: 1919, weight: 5},
-  {start: 1920, end: 1939, weight: 15},
-  {start: 1940, end: 1949, weight: 5},
-  {start: 1950, end: 1969, weight: 15},
-  {start: 1970, end: 1979, weight: 30},
-  {start: 1980, end: 1989, weight: 50},
-  {start: 1990, end: 2017, weight: 125},
-]
