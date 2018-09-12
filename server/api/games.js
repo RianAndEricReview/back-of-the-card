317e91172a987ca5985aaad0480a4153a5a1a114
Eric Garnett <egcodes@gmail.com>
2018-09-12 10:48:07 -0400

    chore(gameAPI): clean up code

--- server/api/games.js
+++ server/api/games.js
@@ -108,130 +108,130 @@ router.post('/:gameId/question', (req, res, next) => {
   if (questionChoices.questionSkeletonKey.year) { whereClause.year = questionChoices.questionSkeletonKey.year }
   if (!isDerived) { whereClause[questionChoices.statCategory] = { [sequelize.Op.ne]: null } }
   //select query based on whether it is a team or player question
-  if(questionChoices.teamOrPlayer === 'wholeTeam'){
+  if (questionChoices.teamOrPlayer === 'wholeTeam') {
     Teams.findAll({
-      where: {year: questionChoices.questionSkeletonKey.year},
+      where: { year: questionChoices.questionSkeletonKey.year },
       attributes: ['year', 'name', questionChoices.statCategory],
       order: (isDerived) ? null : [[sequelize.col(questionChoices.statCategory), questionChoices.mostOrLeast === 'most' ? 'DESC' : 'ASC']],
     })
-    .then(teams => {
-      let teamDataArr = teams.map(team => {
-        return team.dataValues
-      })
-      if (questionChoices.questionType === 'overall') {
-        //add correct answer to question object
-      question.correctAnswer = `${teamDataArr[0].name} ~ ${teamDataArr[0][questionChoices.statCategory]}`
-      question.answers.push(`${teamDataArr[0].name}`)
-      //add incorrect answers to question object
-      let teamIncorrectAnswerIndex = 1
-      while(teamDataArr[teamIncorrectAnswerIndex][questionChoices.statCategory] === teamDataArr[0][questionChoices.statCategory]){
-        teamIncorrectAnswerIndex++
-      }
-      question.answers.push(`${teamDataArr[teamIncorrectAnswerIndex].name}`)
-      question.answers.push(`${teamDataArr[Math.floor(Math.random() * (teamDataArr.length - teamIncorrectAnswerIndex - 1)) + teamIncorrectAnswerIndex + 1].name}`)
-      question.answers.push(`${teamDataArr[Math.floor(Math.random() * (teamDataArr.length - teamIncorrectAnswerIndex - 1)) + teamIncorrectAnswerIndex + 1].name}`)
-      while(question.answers[2] === question.answers[3]){
-        question.answers[3] = `${teamDataArr[Math.floor(Math.random() * (teamDataArr.length - teamIncorrectAnswerIndex - 1)) + teamIncorrectAnswerIndex + 1].name}`
-      }
-      } else if (questionChoices.questionType === 'comparison') {
-        // randomly select team from top half of list for comparison
-        const teamAnswerIndex = Math.floor(Math.random() * Math.floor(teamDataArr.length / 2)) + 1
-        question.correctAnswer = `${teamDataArr[teamAnswerIndex].name} ~ ${teamDataArr[teamAnswerIndex][questionChoices.statCategory]}`
-        question.answers.push(`${teamDataArr[teamAnswerIndex].name}`)
-
-        // choose other possible answers while making sure the other answer choices do not have same stat value as the chosen/correct answer
-        const possibleTeamIncorrectAnswers = teamDataArr.slice(teamAnswerIndex + 1)
-        for (let i = 0; i < 3; i++) {
-          let teamIncorrectAnswerIndex = Math.floor(Math.random() * possibleTeamIncorrectAnswers.length)
-          while (possibleTeamIncorrectAnswers[teamIncorrectAnswerIndex][questionChoices.statCategory] === teamDataArr[teamAnswerIndex][questionChoices.statCategory]) {
-            teamIncorrectAnswerIndex = Math.floor(Math.random() * possibleTeamIncorrectAnswers.length)
+      .then(teams => {
+        let teamDataArr = teams.map(team => {
+          return team.dataValues
+        })
+        if (questionChoices.questionType === 'overall') {
+          //add correct answer to question object
+          question.correctAnswer = `${teamDataArr[0].name} ~ ${teamDataArr[0][questionChoices.statCategory]}`
+          question.answers.push(`${teamDataArr[0].name}`)
+          //add incorrect answers to question object
+          let teamIncorrectAnswerIndex = 1
+          while (teamDataArr[teamIncorrectAnswerIndex][questionChoices.statCategory] === teamDataArr[0][questionChoices.statCategory]) {
+            teamIncorrectAnswerIndex++
           }
-          question.answers.push(`${possibleTeamIncorrectAnswers[teamIncorrectAnswerIndex].name}`)
-        }
-      }
+          question.answers.push(`${teamDataArr[teamIncorrectAnswerIndex].name}`)
+          question.answers.push(`${teamDataArr[Math.floor(Math.random() * (teamDataArr.length - teamIncorrectAnswerIndex - 1)) + teamIncorrectAnswerIndex + 1].name}`)
+          question.answers.push(`${teamDataArr[Math.floor(Math.random() * (teamDataArr.length - teamIncorrectAnswerIndex - 1)) + teamIncorrectAnswerIndex + 1].name}`)
+          while (question.answers[2] === question.answers[3]) {
+            question.answers[3] = `${teamDataArr[Math.floor(Math.random() * (teamDataArr.length - teamIncorrectAnswerIndex - 1)) + teamIncorrectAnswerIndex + 1].name}`
+          }
+        } else if (questionChoices.questionType === 'comparison') {
+          // randomly select team from top half of list for comparison
+          const teamAnswerIndex = Math.floor(Math.random() * Math.floor(teamDataArr.length / 2)) + 1
+          question.correctAnswer = `${teamDataArr[teamAnswerIndex].name} ~ ${teamDataArr[teamAnswerIndex][questionChoices.statCategory]}`
+          question.answers.push(`${teamDataArr[teamAnswerIndex].name}`)
 
-      Question.create(question)
-        .then(createdQuestion => res.status(201).json(createdQuestion))
-    })
-    .catch(next)
-  } else {
-  //To combine stats for players with multiple entries (ex: player was traded, or all time stats):
-  // we group by the playerID and sum the needed stats in attributes
-  Batting.findAll({
-    where: (whereClause !== {}) ? whereClause : null,
-    attributes: attributes,
-    order: (isDerived) ? null : [[sequelize.col(questionChoices.statCategory), questionChoices.mostOrLeast === 'most' ? 'DESC' : 'ASC']],
-    limit: (isDerived || questionChoices.mostOrLeast === 'least') ? null : 100,
-    include: [{ model: People, attributes: ['playerID', 'nameFirst', 'nameLast'] }],
-    group: ['person.playerID']
-  })
-    .then(players => {
-      let playerDataArr = []
-      //Creates the result array of player objects, in the proper order, to be used to select answers.
-      switch (true) {
-        case (questionChoices.timeFrame === 'allTime' && !!isDerived):
-          playerDataArr = players.map(player => {
-            return { ...player.dataValues, [questionChoices.statCategory]: player[questionChoices.statCategory] }
-          }).filter(player => (player.PA >= 3000)).sort((a, b) => { return b[questionChoices.statCategory] - a[questionChoices.statCategory] })
-          break
-        case (questionChoices.timeFrame === 'singleSeason' && !!isDerived):
-          playerDataArr = (questionChoices.mostOrLeast === 'most') ?
-            players.map(player => {
-              return { ...player.dataValues, [questionChoices.statCategory]: player[questionChoices.statCategory] }
-            }).sort((a, b) => { return b[questionChoices.statCategory] - a[questionChoices.statCategory] })
-            : players.map(player => {
-              return { ...player.dataValues, [questionChoices.statCategory]: player[questionChoices.statCategory] }
-            }).sort((a, b) => { return a[questionChoices.statCategory] - b[questionChoices.statCategory] })
-          break
-        case (questionChoices.timeFrame === 'singleSeason' && questionChoices.mostOrLeast === 'least'):
-          let minPA = 502
-          // Used to set the required minimum plate appearances based on the year
-          for (let i = 0; i < minPAPerYear.length; i++) {
-            if (questionChoices.questionSkeletonKey.year >= minPAPerYear[i].start && questionChoices.questionSkeletonKey.year <= minPAPerYear[i].end) {
-              minPA = minPAPerYear[i].minPA
-              break
+          // choose other possible answers while making sure the other answer choices do not have same stat value as the chosen/correct answer
+          const possibleTeamIncorrectAnswers = teamDataArr.slice(teamAnswerIndex + 1)
+          for (let i = 0; i < 3; i++) {
+            let teamIncorrectAnswerIndex = Math.floor(Math.random() * possibleTeamIncorrectAnswers.length)
+            while (possibleTeamIncorrectAnswers[teamIncorrectAnswerIndex][questionChoices.statCategory] === teamDataArr[teamAnswerIndex][questionChoices.statCategory]) {
+              teamIncorrectAnswerIndex = Math.floor(Math.random() * possibleTeamIncorrectAnswers.length)
             }
+            question.answers.push(`${possibleTeamIncorrectAnswers[teamIncorrectAnswerIndex].name}`)
           }
-          playerDataArr = players.map(player => {
+        }
+
+        Question.create(question)
+          .then(createdQuestion => res.status(201).json(createdQuestion))
+      })
+      .catch(next)
+  } else {
+    //To combine stats for players with multiple entries (ex: player was traded, or all time stats):
+    // we group by the playerID and sum the needed stats in attributes
+    Batting.findAll({
+      where: (whereClause !== {}) ? whereClause : null,
+      attributes: attributes,
+      order: (isDerived) ? null : [[sequelize.col(questionChoices.statCategory), questionChoices.mostOrLeast === 'most' ? 'DESC' : 'ASC']],
+      limit: (isDerived || questionChoices.mostOrLeast === 'least') ? null : 100,
+      include: [{ model: People, attributes: ['playerID', 'nameFirst', 'nameLast'] }],
+      group: ['person.playerID']
+    })
+      .then(players => {
+        let playerDataArr = []
+        //Creates the result array of player objects, in the proper order, to be used to select answers.
+        switch (true) {
+          case (questionChoices.timeFrame === 'allTime' && !!isDerived):
+            playerDataArr = players.map(player => {
+              return { ...player.dataValues, [questionChoices.statCategory]: player[questionChoices.statCategory] }
+            }).filter(player => (player.PA >= 3000)).sort((a, b) => { return b[questionChoices.statCategory] - a[questionChoices.statCategory] })
+            break
+          case (questionChoices.timeFrame === 'singleSeason' && !!isDerived):
+            playerDataArr = (questionChoices.mostOrLeast === 'most') ?
+              players.map(player => {
+                return { ...player.dataValues, [questionChoices.statCategory]: player[questionChoices.statCategory] }
+              }).sort((a, b) => { return b[questionChoices.statCategory] - a[questionChoices.statCategory] })
+              : players.map(player => {
+                return { ...player.dataValues, [questionChoices.statCategory]: player[questionChoices.statCategory] }
+              }).sort((a, b) => { return a[questionChoices.statCategory] - b[questionChoices.statCategory] })
+            break
+          case (questionChoices.timeFrame === 'singleSeason' && questionChoices.mostOrLeast === 'least'):
+            let minPA = 502
+            // Used to set the required minimum plate appearances based on the year
+            for (let i = 0; i < minPAPerYear.length; i++) {
+              if (questionChoices.questionSkeletonKey.year >= minPAPerYear[i].start && questionChoices.questionSkeletonKey.year <= minPAPerYear[i].end) {
+                minPA = minPAPerYear[i].minPA
+                break
+              }
+            }
+            playerDataArr = players.map(player => {
               return player.dataValues
             }).filter(player => (player.PA >= minPA))
             break
-        default:
-          playerDataArr = players.map(player => {
-            return player.dataValues
-          })
-      }
-
-      //Build the question object by selecting the correct answer and 3 other answers, and then post the question to DB.
-      if (questionChoices.questionType === 'overall') {
-        const answerIndexArr = [Math.ceil(Math.random() * 5), Math.ceil(Math.random() * 10) + 6, Math.ceil(Math.random() * 15) + 16]
-        question.correctAnswer = `${playerDataArr[0].person.dataValues.nameFirst} ${playerDataArr[0].person.dataValues.nameLast} ~ ${playerDataArr[0][questionChoices.statCategory]}`
-        question.answers.push(`${playerDataArr[0].person.dataValues.nameFirst} ${playerDataArr[0].person.dataValues.nameLast}`)
-        answerIndexArr.forEach(answerIndex => {
-          question.answers.push(`${playerDataArr[answerIndex].person.dataValues.nameFirst} ${playerDataArr[answerIndex].person.dataValues.nameLast}`)
-        })
-      } else if (questionChoices.questionType === 'comparison') {
-        // randomly choose a player for comparison
-        const correctAnswerIndex = Math.floor(Math.random() * 26) + 4
-        question.correctAnswer = `${playerDataArr[correctAnswerIndex].person.dataValues.nameFirst} ${playerDataArr[correctAnswerIndex].person.dataValues.nameLast} ~ ${playerDataArr[correctAnswerIndex][questionChoices.statCategory]}`
-        question.answers.push(`${playerDataArr[correctAnswerIndex].person.dataValues.nameFirst} ${playerDataArr[correctAnswerIndex].person.dataValues.nameLast}`)
-
-        // create a list of possible other players for comparison
-        const possibleIncorrectAnswers = (playerDataArr.length - correctAnswerIndex + 1 > 70) ? playerDataArr.slice(correctAnswerIndex + 1, correctAnswerIndex + 71) : playerDataArr
-        const answerIndexParameter = Math.floor(possibleIncorrectAnswers.length / 3)
-
-        // choose other possible answers while making sure the other answer choices do not have the same stat as chosen answer player
-        for (let i = 0; i < 3; i++) {
-          let incorrectAnswerIndex = Math.floor(Math.random() * answerIndexParameter) + (answerIndexParameter * i)
-          while (playerDataArr[correctAnswerIndex][questionChoices.statCategory] === possibleIncorrectAnswers[incorrectAnswerIndex][questionChoices.statCategory]) {
-            incorrectAnswerIndex = Math.floor(Math.random() * answerIndexParameter) + (answerIndexParameter * i)
-          }
-          question.answers.push(`${possibleIncorrectAnswers[incorrectAnswerIndex].person.dataValues.nameFirst} ${possibleIncorrectAnswers[incorrectAnswerIndex].person.dataValues.nameLast}`)
+          default:
+            playerDataArr = players.map(player => {
+              return player.dataValues
+            })
         }
-      }
-      Question.create(question)
-        .then(createdQuestion => res.status(201).json(createdQuestion))
-    })
-    .catch(next)
+
+        //Build the question object by selecting the correct answer and 3 other answers, and then post the question to DB.
+        if (questionChoices.questionType === 'overall') {
+          const answerIndexArr = [Math.ceil(Math.random() * 5), Math.ceil(Math.random() * 10) + 6, Math.ceil(Math.random() * 15) + 16]
+          question.correctAnswer = `${playerDataArr[0].person.dataValues.nameFirst} ${playerDataArr[0].person.dataValues.nameLast} ~ ${playerDataArr[0][questionChoices.statCategory]}`
+          question.answers.push(`${playerDataArr[0].person.dataValues.nameFirst} ${playerDataArr[0].person.dataValues.nameLast}`)
+          answerIndexArr.forEach(answerIndex => {
+            question.answers.push(`${playerDataArr[answerIndex].person.dataValues.nameFirst} ${playerDataArr[answerIndex].person.dataValues.nameLast}`)
+          })
+        } else if (questionChoices.questionType === 'comparison') {
+          // randomly choose a player for comparison
+          const correctAnswerIndex = Math.floor(Math.random() * 26) + 4
+          question.correctAnswer = `${playerDataArr[correctAnswerIndex].person.dataValues.nameFirst} ${playerDataArr[correctAnswerIndex].person.dataValues.nameLast} ~ ${playerDataArr[correctAnswerIndex][questionChoices.statCategory]}`
+          question.answers.push(`${playerDataArr[correctAnswerIndex].person.dataValues.nameFirst} ${playerDataArr[correctAnswerIndex].person.dataValues.nameLast}`)
+
+          // create a list of possible other players for comparison
+          const possibleIncorrectAnswers = (playerDataArr.length - correctAnswerIndex + 1 > 70) ? playerDataArr.slice(correctAnswerIndex + 1, correctAnswerIndex + 71) : playerDataArr
+          const answerIndexParameter = Math.floor(possibleIncorrectAnswers.length / 3)
+
+          // choose other possible answers while making sure the other answer choices do not have the same stat as chosen answer player
+          for (let i = 0; i < 3; i++) {
+            let incorrectAnswerIndex = Math.floor(Math.random() * answerIndexParameter) + (answerIndexParameter * i)
+            while (playerDataArr[correctAnswerIndex][questionChoices.statCategory] === possibleIncorrectAnswers[incorrectAnswerIndex][questionChoices.statCategory]) {
+              incorrectAnswerIndex = Math.floor(Math.random() * answerIndexParameter) + (answerIndexParameter * i)
+            }
+            question.answers.push(`${possibleIncorrectAnswers[incorrectAnswerIndex].person.dataValues.nameFirst} ${possibleIncorrectAnswers[incorrectAnswerIndex].person.dataValues.nameLast}`)
+          }
+        }
+        Question.create(question)
+          .then(createdQuestion => res.status(201).json(createdQuestion))
+      })
+      .catch(next)
   }
 })
