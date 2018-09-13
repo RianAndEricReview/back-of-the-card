530b39735d7f4ce2c0cf6ac8654848b45a675c9f
Rian Halperin <rianhalperin@gmail.com>
2018-09-13 13:19:47 -0400

    feat(gameplayFunctions): create constructor for question query parameters

---
+++ GameplayFunctions/questions/questionQueryGenerator.js
@@ -0,0 +1,11 @@
+//THIS FILE CONTAINS THE CONSTRUCTOR FUNCTION FOR GENERATING DYNAMIC QUERY PARAMETERS
+
+//class to create an object with query parameters
+class QuestionQueryParameters {
+  constructor() {
+    this.attributes = []
+  }
+
+}
+
+module.exports = {QuestionQueryParameters}
