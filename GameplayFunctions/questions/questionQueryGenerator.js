b6a926accf5d262a4c6899dd0de11d575e640129
Rian Halperin <rianhalperin@gmail.com>
2018-09-13 14:06:59 -0400

    refactor(gameplayFunction): add includeGenerator method to QQP constructor

--- GameplayFunctions/questions/questionQueryGenerator.js
+++ GameplayFunctions/questions/questionQueryGenerator.js
@@ -1,14 +1,20 @@
 //THIS FILE CONTAINS THE CONSTRUCTOR FUNCTION FOR GENERATING DYNAMIC QUERY PARAMETERS
+const sequelize = require('sequelize')
+const { Batting, People} = require('../../server/db/models')
 
 //class to create an object with query parameters
 class QuestionQueryParameters {
 
   //Method to set the where parameters for the query based on the questionChoices object
-  whereGenerator(questionChoices, isDerived, sequelize){
+  whereGenerator(questionChoices, isDerived){
     const whereClause = {}
     if (questionChoices.questionSkeletonKey.year) { whereClause.year = questionChoices.questionSkeletonKey.year }
     if (!isDerived) { whereClause[questionChoices.statCategory] = { [sequelize.Op.ne]: null } }
     if (whereClause !== {}) { this.where = whereClause }
+  }
+
+  includeGenerator(questionChoices){
+    if (questionChoices.teamOrPlayer === 'singlePlayer') {this.include = [{ model: People, attributes: ['playerID', 'nameFirst', 'nameLast'] }]}
   }
 
 }
