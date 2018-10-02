10365bf37ad357a04597aa09634ea2d5ca7c3eab
Rian Halperin <rianhalperin@gmail.com>
2018-10-02 10:36:26 -0400

    chore(GameBoardPres): remove temp code

--- client/components/game/GameBoardPres.js
+++ client/components/game/GameBoardPres.js
@@ -1,19 +1,10 @@
 import React from 'react'
-import {QuestionChoices} from '../../../GameplayFunctions/questions/questionGenerator'
-import {defaultYearRanges} from '../../../GameplayFunctions/questions/content/questionContent'
-import {teamOrPlayer} from '../../../GameplayFunctions/questions/content/questionOptionsContent'
-import {questionTextGenerator} from '../../../GameplayFunctions/questions/questionHelperFuncs'
 /* eslint-disable react/display-name */
-/* eslint-disable react/display-name */
+
 export default () => {
-  // const selectedQuestionChoices = new QuestionChoices()
-  // console.log('!!!!!!!!!!!', selectedQuestionChoices)
-  // selectedQuestionChoices.questionChoiceGenerator(teamOrPlayer, defaultYearRanges)
-  // const questionText = questionTextGenerator(selectedQuestionChoices)
   return (
     <div className="gameboard-pres-container">
       <h1> Playing the Game!!!!! </h1>
-      {<h1>questionText</h1>}
     </div>
   )
 }
