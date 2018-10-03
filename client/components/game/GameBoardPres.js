eb24b74897f2043b55190d0b8086c1c113e0c369
Rian Halperin <rianhalperin@gmail.com>
2018-10-03 12:15:39 -0400

    style(GameBoardPres): format button layout

--- client/components/game/GameBoardPres.js
+++ client/components/game/GameBoardPres.js
@@ -5,13 +5,17 @@ export default (props) => {
   const currentQuestion = props.questions.find((question) => question.questionNum === props.currentQuestionNum)
   return (
     <div className="gameboard-pres-container">
-      <h1> BACK OF THE CARD </h1>
-      <h5><strong>Question {props.currentQuestionNum}/{props.numOfQuestions}:</strong> {currentQuestion.question}</h5>
+    <h1 id="gameplay-logo"> BackOfTheCard </h1>
+    <div id="gameboard-question">
+      <h4><strong>Question {props.currentQuestionNum}/{props.numOfQuestions}:</strong> {currentQuestion.question}</h4>
+    </div>
       <div className="container">
-        <div className="row">{currentQuestion.answers.map((answer, index) => {
+        <div className="row" id="gameboard-answer-container">{currentQuestion.answers.map((answer, index) => {
           const buttonTypes = ["primary", "danger", "success", "warning"]
           return (
-            <div key={index} className={`col-6 btn btn-outline-${buttonTypes[index]}`}> {answer} </div>
+            <div key={index} className="col-6 text-center" id="gameboard-answer-grid">
+              <div className={`gameboard-answer-buttons btn btn-outline-${buttonTypes[index]}`}> {answer} </div>
+            </div>
           )
         }
         )}
