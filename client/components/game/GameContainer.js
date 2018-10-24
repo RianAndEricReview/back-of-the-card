58f2f53f5c8ad4726df8b4ceac778e9981b6edfd
Rian Halperin <rianhalperin@gmail.com>
2018-10-24 12:59:48 -0400

    feat(GameContainer): Dispatch playerAnswer and playerQuestionResult on submit

--- client/components/game/GameContainer.js
+++ client/components/game/GameContainer.js
@@ -4,14 +4,13 @@ import { withRouter } from 'react-router-dom'
 import LoadingPres from './LoadingPres'
 import IndividualPlayerPres from './IndividualPlayerPres'
 import GameBoardPres from './GameBoardPres'
-import { getAllPlayersThunk, createAllQuestionsThunk, getAllQuestionsThunk } from '../../store'
+import { getAllPlayersThunk, createAllQuestionsThunk, getAllQuestionsThunk, createQuestionResult, addPlayerAnswer } from '../../store'
 
 export class GameContainerClass extends Component {
   constructor(props) {
     super(props)
     this.state = {
       clickedAnswer: '',
-      playerQuestionResult: {answer: '', time: 0, questionId: null}
     }
 
     this.answerButtonClick = this.answerButtonClick.bind(this)
@@ -25,13 +24,17 @@ export class GameContainerClass extends Component {
 
   answerSubmission(event) {
     event.preventDefault()
+    let playerQuestionResult = {answer: this.state.clickedAnswer, time: 5, questionId: this.props.game.currentQuestion}
     let playerAnswer = {answer: this.state.clickedAnswer, score: 0, gamePlayer: this.props.user.id}
-    let playerQuestionResult = {answer: this.state.clickedAnswer, time: 1, questionId: this.props.game.currentQuestion}
-    //The below code is a temporary score generator with minimal functionality. This functionality will be moved to GameplayFunctions and expanded upon to take into account time and gametype.
+
+    //The below section of code is a temporary score generator with minimal functionality. 
+    //This functionality will be moved to GameplayFunctions and expanded upon to take into account time and gametype.
     let correctAnswer = this.props.questions.find(question => this.props.game.currentQuestion === question.questionNum).correctAnswer
     correctAnswer = correctAnswer.slice(0, correctAnswer.indexOf(' ~'))
-    playerQuestionResult.score = playerQuestionResult.answer === correctAnswer ? 1 : 0
-    this.setState({playerQuestionResult})
+    playerAnswer.score = playerAnswer.answer === correctAnswer ? 1 * playerQuestionResult.time : 0
+
+    this.props.createQuestionResult(playerQuestionResult)
+    this.props.addPlayerAnswer(playerAnswer)
   }
 
   componentDidMount() {
@@ -42,7 +45,6 @@ export class GameContainerClass extends Component {
   }
 
   render() {
-    console.log('STATEEEEEEEE', this.state)
     return (
       <div className="game-container">
         {(this.props.game.open || this.props.questions.length <= 0) ? <LoadingPres /> : <GameBoardPres questions={this.props.questions} currentQuestionNum={this.props.game.currentQuestion} numOfQuestions={this.props.game.gametype.numOfQuestions} answerButtonClick={this.answerButtonClick} answerSubmission={this.answerSubmission} />}
@@ -76,6 +78,12 @@ const mapDispatchToProps = dispatch => {
     },
     getAllQuestions(gameId) {
       dispatch(getAllQuestionsThunk(gameId))
+    },
+    createQuestionResult(playerQuestionResult) {
+      dispatch(createQuestionResult(playerQuestionResult))
+    },
+    addPlayerAnswer(playerAnswer) {
+      dispatch(addPlayerAnswer(playerAnswer))
     }
   }
 }
