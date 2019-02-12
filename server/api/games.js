93204175ccf70346ae80efc2216e558889b66548
Eric Garnett <egcodes@gmail.com>
2019-02-12 10:42:52 -0500

    ref(gamesAPI): move question creation route functionality to game creation route

--- server/api/games.js
+++ server/api/games.js
@@ -50,6 +50,146 @@ router.post('/', (req, res, next) => {
         .then(game => {
           game.dataValues.gametype = gametype
           res.status(201).json(game)
+
+          const numOfQuestions = gametype.numOfQuestions
+          const questionTexts = []
+          const questionInfoArr = []
+
+          //create the proper number of questions for a game based on the # of questions stated in the req.
+          for (let i = 1; i <= numOfQuestions; i++) {
+            //generate and populate questionChoices, questionText, question object
+            let questionChoices = {}
+            let question
+            //check to make sure we don't include a question twice in the same game.
+            while (!question || questionTexts.includes(question.question)) {
+              questionChoices = new QuestionChoices()
+              questionChoices.questionChoiceGenerator(firstOption, defaultYearRanges)
+              //TO REMOVE AFTER LEAST CONTENT IS UPDATED - currently prevents situations where all query results are invalid.
+              if (questionChoices.timeFrame === 'allTime') {
+                questionChoices.mostOrLeast = 'most'
+                questionChoices.questionSkeletonKey.mostOrLeast = ['most']
+              }
+
+              question = new QuestionObjectGenerator(game.id, i)
+              question.questionTextGenerator(questionChoices)
+            }
+            questionTexts.push(question.question)
+
+            //Check derived content and set variable if chosen stat category is derived.
+            let isDerived = derivedBattingStats.find((stat) => {
+              return questionChoices.statCategory === stat.statCat
+            })
+
+            //Create and populate the object that will be passed into the query
+            const QQP = new QuestionQueryParameters()
+            Object.getOwnPropertyNames(QQP.constructor.prototype).forEach(method => {
+              if (method !== 'constructor') {
+                QQP[method](questionChoices, isDerived)
+              }
+            })
+
+            //select table to query based on whether it is a team or player question
+            //as we update the logic to add additional types like pitching or individual team, this might be better off as a switch statement.
+            let table = ''
+            if (questionChoices.teamOrPlayer === 'wholeTeam') { table = Teams }
+            else if (questionChoices.teamOrPlayer === 'singlePlayer') { table = Batting }
+
+            // Info needed to build a question object
+            const findAllInfo = { QQP, questionChoices, isDerived, table, question }
+            findAllInfoArr.push(findAllInfo)
+          }
+
+          const questionCreatorFunc = (gameId, questionInfoArr) => {
+            Promise.all(questionInfoArr.map(findInfo => findInfo.table.findAll({ ...findInfo.QQP })))
+              .then(foundInfo => {
+                const questionsArr = []
+                foundInfo.forEach((data, idx) => {
+                  let consolidatedDataArr
+                  if (!data.length) {
+                    dataIsGood = false
+                    console.log('111111111111111')
+                  } else {
+                    //run the data consolidator
+                    consolidatedDataArr = dataConsolidator(data, questionInfoArr[idx].questionChoices, questionInfoArr[idx].isDerived)
+
+                    // for MOST questions check to make sure data is valid
+                    if (questionInfoArr[idx].questionChoices.mostOrLeast === 'most') {
+                      console.log('most')
+                      // make sure there are enough data points after the sixth to make a good question
+                      const failsafePlayer = consolidatedDataArr[5]
+                      const firstValidPlayerIdx = consolidatedDataArr.findIndex(player => failsafePlayer[questionInfoArr[idx].questionChoices.statCategory] !== player[questionInfoArr[idx].questionChoices.statCategory])
+                      if (consolidatedDataArr.slice(firstValidPlayerIdx).length < 30) {
+                        console.log('33333333333333333')
+                        dataIsGood = false
+                      }
+
+                      //check first 10 fail if any are nulls or 0s
+                      for (let j = 0; j < 10; j++) {
+                        if (consolidatedDataArr[j][questionInfoArr[idx].questionChoices.statCategory] === '0') {
+                          console.log('44444444444444444444')
+                          dataIsGood = false
+                          break
+                        }
+                      }
+
+                      //if overall, fail if the first 6 values are the same
+                      if (dataIsGood && consolidatedDataArr.length < 30 && questionInfoArr[idx].questionChoices.questionType === 'overall' && (consolidatedDataArr[0][questionInfoArr[idx].questionChoices.statCategory] === consolidatedDataArr[5][questionInfoArr[idx].questionChoices.statCategory])) {
+                        console.log('55555555555555555555555555')
+                        dataIsGood = false
+                      }
+                    }
+
+                    // for LEAST questions check to make sure data is valid
+                    else if (questionInfoArr[idx].questionChoices.mostOrLeast === 'least') {
+                      console.log('least')
+                      const failsafePlayer = consolidatedDataArr[0]
+                      const firstValidPlayerIdx = consolidatedDataArr.findIndex(player => failsafePlayer[questionInfoArr[idx].questionChoices.statCategory] !== player[questionInfoArr[idx].questionChoices.statCategory])
+                      if (consolidatedDataArr.slice(firstValidPlayerIdx).length < 30) {
+                        console.log('777777777777777777')
+                        dataIsGood = false
+                      }
+                    }
+                  }
+
+                  if (dataIsGood) {
+                    console.log('inside of data is good')
+                    // Generate questionObject answers
+                    questionInfoArr[idx].question.questionAnswerGenerator(questionInfoArr[idx].questionChoices, consolidatedDataArr)
+                    questionsArr.push(questionInfoArr[idx].question)
+
+                    //if answers comes back as empty array pick a new year for the query
+                    if (questionInfoArr[idx].question.answers === []) {
+                      console.log('inside of empty answer array')
+                      const newRandomYear = randomYearSelector(defaultYearRanges)
+                      questionInfoArr[idx].questionChoices.questionSkeletonKey.year = newRandomYear
+                      questionInfoArr[idx].QQP.where.year = newRandomYear
+                      dataIsGood = false
+                    }
+                  }
+
+                  //if data is not good get another random year
+                  if (!dataIsGood) {
+                    console.log('inside of bad data if')
+                    //*******TODO update the database with the year and stat of bad data
+
+                    //use the idx to get the correct findAllInfo object from the questionInfoArr
+                    //Update the year in all spots in the (question choices/skeleton key and the QQP)
+                    const newRandomYear = randomYearSelector(defaultYearRanges)
+                    questionInfoArr[idx].questionChoices.questionSkeletonKey.year = newRandomYear
+                    questionInfoArr[idx].QQP.where.year = newRandomYear
+                  }
+                })
+
+                if (dataIsGood) {
+                  // Post the questions to DB and send results to front end
+                  Promise.all(questionsArr.map(question => Question.create(question)))
+                    .then(questions => {
+                      res.status(201).json(questions)
+                    })
+                }
+              })
+              .catch(next)
+          }
           return GamePlayer.create({ gameId: game.id, userId: req.body.playerId.toString() })
         })
     })
@@ -73,158 +213,6 @@ router.put('/:gameId/addNewPlayer', (req, res, next) => {
         })
     })
     .catch(next)
-})
-
-// Used to generate a question instance
-router.post('/:gameId/questions', (req, res, next) => {
-  const numOfQuestions = req.body.numOfQuestions
-  const questionTexts = []
-  const questionInfoArr = []
-
-  //create the proper number of questions for a game based on the # of questions stated in the req.
-  for (let i = 1; i <= numOfQuestions; i++) {
-    //generate and populate questionChoices, questionText, question object
-    let questionChoices = {}
-    let question
-    //check to make sure we don't include a question twice in the same game.
-    while (!question || questionTexts.includes(question.question)) {
-      questionChoices = new QuestionChoices()
-      questionChoices.questionChoiceGenerator(firstOption, defaultYearRanges)
-      //TO REMOVE AFTER LEAST CONTENT IS UPDATED - currently prevents situations where all query results are invalid.
-      if (questionChoices.timeFrame === 'allTime') {
-        questionChoices.mostOrLeast = 'most'
-        questionChoices.questionSkeletonKey.mostOrLeast = ['most']
-      }
-
-      question = new QuestionObjectGenerator(req.params.gameId, i)
-      question.questionTextGenerator(questionChoices)
-    }
-    questionTexts.push(question.question)
-
-    //Check derived content and set variable if chosen stat category is derived.
-    let isDerived = derivedBattingStats.find((stat) => {
-      return questionChoices.statCategory === stat.statCat
-    })
-
-    //Create and populate the object that will be passed into the query
-    const QQP = new QuestionQueryParameters()
-    Object.getOwnPropertyNames(QQP.constructor.prototype).forEach(method => {
-      if (method !== 'constructor') {
-        QQP[method](questionChoices, isDerived)
-      }
-    })
-
-    //select table to query based on whether it is a team or player question
-    //as we update the logic to add additional types like pitching or individual team, this might be better off as a switch statement.
-    let table = ''
-    if (questionChoices.teamOrPlayer === 'wholeTeam') { table = Teams }
-    else if (questionChoices.teamOrPlayer === 'singlePlayer') { table = Batting }
-
-    // Info needed to build a question object
-    const findAllInfo = { QQP, questionChoices, isDerived, table, question }
-    questionInfoArr.push(findAllInfo)
-  }
-
-  //set up a goodData variable to be used to continue running a while loop until getting good data set
-  let dataIsGood
-  //set a do while loop that keeps running while dataIsGood is false
-  do {
-    //each new data set is assumed true until proven otherwise
-    dataIsGood = true
-    // Set up as nested Promise.alls to collect all questions before sending them to the front end
-    Promise.all(questionInfoArr.map(findInfo => findInfo.table.findAll({ ...findInfo.QQP })))
-      .then(foundInfo => {
-        const questionsArr = []
-        foundInfo.forEach((data, idx) => {
-          let consolidatedDataArr
-          if (!data.length) {
-            dataIsGood = false
-            console.log('111111111111111')
-          } else {
-            //run the data consolidator
-            consolidatedDataArr = dataConsolidator(data, questionInfoArr[idx].questionChoices, questionInfoArr[idx].isDerived)
-
-            // for MOST questions check to make sure data is valid
-            if (questionInfoArr[idx].questionChoices.mostOrLeast === 'most') {
-              console.log('most')
-              // make sure there are enough data points after the sixth to make a good question
-              const failsafePlayer = consolidatedDataArr[5]
-              const firstValidPlayerIdx = consolidatedDataArr.findIndex(player => failsafePlayer[questionInfoArr[idx].questionChoices.statCategory] !== player[questionInfoArr[idx].questionChoices.statCategory])
-              if (consolidatedDataArr.slice(firstValidPlayerIdx).length < 30) {
-                console.log('33333333333333333')
-                dataIsGood = false
-              }
-
-              //check first 10 fail if any are nulls or 0s
-              for (let j = 0; j < 10; j++) {
-                if (consolidatedDataArr[j][questionInfoArr[idx].questionChoices.statCategory] === '0') {
-                  console.log('44444444444444444444')
-                  dataIsGood = false
-                  break
-                }
-              }
-
-              //if overall, fail if the first 6 values are the same
-              if (dataIsGood && consolidatedDataArr.length < 30 && questionInfoArr[idx].questionChoices.questionType === 'overall' && (consolidatedDataArr[0][questionInfoArr[idx].questionChoices.statCategory] === consolidatedDataArr[5][questionInfoArr[idx].questionChoices.statCategory])) {
-                console.log('55555555555555555555555555')
-                dataIsGood = false
-              }
-            }
-
-            // for LEAST questions check to make sure data is valid
-            else if (questionInfoArr[idx].questionChoices.mostOrLeast === 'least') {
-              console.log('least')
-              const failsafePlayer = consolidatedDataArr[0]
-              const firstValidPlayerIdx = consolidatedDataArr.findIndex(player => failsafePlayer[questionInfoArr[idx].questionChoices.statCategory] !== player[questionInfoArr[idx].questionChoices.statCategory])
-              if (consolidatedDataArr.slice(firstValidPlayerIdx).length < 30) {
-                console.log('777777777777777777')
-                dataIsGood = false
-              }
-            }
-          }
-
-          if (dataIsGood) {
-            console.log('inside of data is good')
-            // Generate questionObject answers
-            questionInfoArr[idx].question.questionAnswerGenerator(questionInfoArr[idx].questionChoices, consolidatedDataArr)
-            questionsArr.push(questionInfoArr[idx].question)
-
-            //if answers comes back as empty array pick a new year for the query
-            if (questionInfoArr[idx].question.answers === []) {
-              console.log('inside of empty answer array')
-              const newRandomYear = randomYearSelector(defaultYearRanges)
-              questionInfoArr[idx].questionChoices.questionSkeletonKey.year = newRandomYear
-              questionInfoArr[idx].QQP.where.year = newRandomYear
-              dataIsGood = false
-            }
-          }
-
-          //if data is not good get another random year
-          if (!dataIsGood) {
-            console.log('inside of bad data if')
-            //*******TODO update the database with the year and stat of bad data
-
-            //use the idx to get the correct findAllInfo object from the questionInfoArr
-            //Update the year in all spots in the (question choices/skeleton key and the QQP)
-            const newRandomYear = randomYearSelector(defaultYearRanges)
-            questionInfoArr[idx].questionChoices.questionSkeletonKey.year = newRandomYear
-            questionInfoArr[idx].QQP.where.year = newRandomYear
-          }
-        })
-
-        if (dataIsGood) {
-          // Post the questions to DB and send results to front end
-          Promise.all(questionsArr.map(question => Question.create(question)))
-            .then(questions => {
-              res.status(201).json(questions)
-            })
-        }
-      })
-      .catch(next)
-
-  }
-  //if good data set goodData to true (ends the loop)
-  while (!dataIsGood)
 })
 
 // Used to fetch all questions of a specific game instanace
