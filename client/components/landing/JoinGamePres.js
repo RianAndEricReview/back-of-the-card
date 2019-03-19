19537f9f625c4ded345dd689710446fff722311b
Rian Halperin <rianhalperin@gmail.com>
2019-03-19 12:39:27 -0400

    style(landing): update images

--- client/components/landing/JoinGamePres.js
+++ client/components/landing/JoinGamePres.js
@@ -5,12 +5,12 @@ export default (props) => {
     <div>
       <div id="landing-game-container">
         <div id="landing-game-content">
-          <div>
+          <div onClick={(e) => props.joinGameClick(e, props.gametypeId, props.playerId, props.gametypes)}>
             <img src={props.gametypeImage} className="landing-gametype-image" />
             <p className="centered-gametype"> {props.gametypeName} </p>
-            <p> {props.gametypeDescription} </p>
+            {/* <p> {props.gametypeDescription} </p> */}
             <div className="center-container">
-              <button className="btn play-button" onClick={(e) => props.joinGameClick(e, props.gametypeId, props.playerId, props.gametypes)}>PLAY</button>
+              {/* <button className="btn play-button" onClick={(e) => props.joinGameClick(e, props.gametypeId, props.playerId, props.gametypes)}>PLAY</button> */}
             </div>
           </div>
         </div>
--- client/components/landing/LandingPres.js
+++ client/components/landing/LandingPres.js
@@ -3,16 +3,16 @@ import React from 'react'
 export default () => {
   return (
     <div className="landing-pres-container">
-      <h1>Welcome to Back of The Card Baseball Trivia!</h1>
+      <img id="baseball-card-collage" src="../../../images/IMG_0118(3).jpg" />
+      <h1>BACK OF THE CARD</h1>
       <div id="landing-content-container">
         <div id="landing-content">
           <div>
-          <img src="../../../images/baseball-card-collage.jpg" />
-          <p>Back of the Card is a fresh new baseball trivia game.
-          Revisit the many hours you spent in your youth reading over the stats on the backs of your favorite baseball cards.
-          Put that knowledge to the test in an exciting fast paced trivia competition against live players from around the globe and rise up the leader boards.
+            <p>Back of the Card is a fresh new baseball trivia game.
+            Revisit the many hours you spent in your youth reading over the stats on the backs of your favorite baseball cards.
+            Put that knowledge to the test in an exciting fast paced trivia competition against live players from around the globe and rise up the leader boards.
         </p>
-        </div>
+          </div>
         </div>
       </div>
     </div>
