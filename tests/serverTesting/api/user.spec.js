fe6bae78da59ffc090a50eb955476100467dfa21
Eric Garnett <egcodes@gmail.com>
2019-06-05 11:30:37 -0400

    test(user.specs): restore user tests

--- tests/serverTesting/api/user.spec.js
+++ tests/serverTesting/api/user.spec.js
@@ -1,25 +1,25 @@
-// const { expect } = require('chai')
-// const request = require('supertest')
-// const db = require('../../../server/db')
-// const app = require('../../../server/index')
-// const User = db.model('user')
+const { expect } = require('chai')
+const request = require('supertest')
+const db = require('../../../server/db')
+const app = require('../../../server/index')
+const User = db.model('user')
 
 
-// describe('User routes', () => {
-//   beforeEach(() => db.sync({ force: true }))
+describe('User routes', () => {
+  beforeEach(() => db.sync({ force: true }))
 
-//   describe('/api/users/', () => {
-//     const bobsEmail = 'bob@bob.bob'
-//     const bobsFirstName = 'Bob'
-//     const bobsLastName = 'Bob'
+  describe('/api/users/', () => {
+    const bobsEmail = 'bob@bob.bob'
+    const bobsFirstName = 'Bob'
+    const bobsLastName = 'Bob'
 
-//     beforeEach(() => User.create({ email: bobsEmail, firstName: bobsFirstName, lastName: bobsLastName }))
-//     it('GET /api/users', () => {
-//       return request(app).get('/api/users').expect(200)
-//         .then(res => {
-//           expect(res.body).to.be.an('array')
-//           expect(res.body[0].email).to.be.equal(bobsEmail)
-//         })
-//     })
-//   })
-// })
+    beforeEach(() => User.create({ email: bobsEmail, firstName: bobsFirstName, lastName: bobsLastName }))
+    it('GET /api/users', () => {
+      return request(app).get('/api/users').expect(200)
+        .then(res => {
+          expect(res.body).to.be.an('array')
+          expect(res.body[0].email).to.be.equal(bobsEmail)
+        })
+    })
+  })
+})
