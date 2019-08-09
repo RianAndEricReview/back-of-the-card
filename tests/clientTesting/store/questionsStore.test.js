c83500979cc7bd5d64ca6c8da35a80ca38316431
Eric Garnett <egcodes@gmail.com>
2019-08-09 11:40:22 -0400

    test(questionsStore.test): implement test for clearAllQuestions action

--- tests/clientTesting/store/questionsStore.test.js
+++ tests/clientTesting/store/questionsStore.test.js
@@ -2,6 +2,7 @@ import * as questionsStore from '../../../client/store/questions'
 
 describe('questions store - actions, thunk, and reducer', () => {
   const questions = [{id: 1, question: 'How much wood would a woodchuck chuck if a woodchuck could chuck wood?'}, {id: 2, question: 'Why is the sky blue?'}]
+
   describe('actions', () => {
     it('should create an action to add the questions', () => {
       const expectedAction = {
@@ -9,6 +10,12 @@ describe('questions store - actions, thunk, and reducer', () => {
         questions
       }
       expect(questionsStore.getAllQuestions(questions)).toEqual(expectedAction)
+    })
+    it('should create an action to clear the questions', () => {
+      const expectedAction = {
+        type: questionsStore.CLEAR_ALL_QUESTIONS
+      }
+      expect(questionsStore.clearAllQuestions()).toEqual(expectedAction)
     })
   })
 })
