6dc559a41da3bd02847b15dfd89e6681a05c5d0a
Eric Garnett <egcodes@gmail.com>
2019-08-09 13:16:03 -0400

    test(questionsStore.test): implement getAllQ uestionsThunk test

--- tests/clientTesting/store/questionsStore.test.js
+++ tests/clientTesting/store/questionsStore.test.js
@@ -1,7 +1,14 @@
+import MockAdapter from 'axios-mock-adapter'
+import axios from 'axios'
+import configureMockStore from 'redux-mock-store'
+import thunk from 'redux-thunk'
 import * as questionsStore from '../../../client/store/questions'
 
+const middlewares = [thunk]
+const mockStore = configureMockStore(middlewares)
+
 describe('questions store - actions, thunk, and reducer', () => {
-  const questions = [{id: 1, question: 'How much wood would a woodchuck chuck if a woodchuck could chuck wood?'}, {id: 2, question: 'Why is the sky blue?'}]
+  const questions = [{ id: 1, question: 'How much wood would a woodchuck chuck if a woodchuck could chuck wood?' }, { id: 2, question: 'Why is the sky blue?' }]
 
   describe('actions', () => {
     it('should create an action to add the questions', () => {
@@ -18,4 +25,27 @@ describe('questions store - actions, thunk, and reducer', () => {
       expect(questionsStore.clearAllQuestions()).toEqual(expectedAction)
     })
   })
+
+  describe('getAllQuestionsThunk', () => {
+    const store = mockStore({ defaultQuestions: [] })
+    const mockAxios = new MockAdapter(axios)
+
+    afterEach(() => {
+      store.clearActions()
+    })
+    it('creates and dispatches the GET_ALL_QUESTIONS action', () => {
+      mockAxios.onGet('/api/games/1/questions').replyOnce(200, questions)
+      store.dispatch(questionsStore.getAllQuestionsThunk(1))
+        .then(() => {
+          const expectedActions = [{
+            type: questionsStore.GET_ALL_QUESTIONS,
+            questions
+          }]
+          expect(store.getActions()).toEqual(expectedActions)
+        })
+    })
+  })
 })
+
+// REMINDER TO PUT THIS BACK IN THE TEST SCRIPT
+// mocha ./tests/serverTesting/**/*.spec.js --require @babel/polyfill --require @babel/register &&
